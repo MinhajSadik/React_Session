b29747d79ff7337ebd6bcde4c2a45b9fd093a908
MinhajSadik <MinhajoSadik@gmail.com>
2023-01-03 09:21:12 +0600

    [ADD] nothing just simple worked changed

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import Logo from "../Logo/Logo";
 
 export default function Navbar() {
+  var age = 17;
   return (
     <div>
+      {age === 18 ? "older" : age === 17 ? "roughly" : "not a number"}
+      <input type="text" />
+      <button>Focus the input</button>
       <Logo />
     </div>
   );
