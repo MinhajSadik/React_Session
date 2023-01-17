3dfb8ea066ce5eda4e44919e0b371d47652cdb7c
MinhajSadik <MinhajoSadik@gmail.com>
2023-01-17 11:11:57 +0600

    [DONE] useReducer advanced practice

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import React, { useReducer } from 'react';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import ThemeProvider from './contexts/ThemeProvider';
-import UserProvider from './contexts/UserProvider';
-
-const initState = 2
-const countReducer = (state = 0, action) => {
-  console.log(state)
-}
+import React from 'react';
 
 
 function App() {
-  const [countReducer, dispatch] = useReducer(countReducer, initState)
 
   return (
-    <>
-      <ThemeProvider>
-        <UserProvider>
-
-
-          <Router>
-            <Routes>
-
-            </Routes>
-          </Router>
-        </UserProvider>
-      </ThemeProvider >
-    </>
+    <div>
+      <h1>
+        Hi
+      </h1>
+    </div>
   );
 }
 
