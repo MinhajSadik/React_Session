cae9cde556551e60c8bf2995bffc21a54e08a150
MinhajSadik <MinhajoSadik@gmail.com>
2023-01-28 21:07:52 +0600

    [DONE] new branch created and deleted all folder

--- src/App.js
+++ src/App.js
@@ -1,39 +1,31 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useReducer } from 'react';
 import './App.css';
-import Loader from './Components/Shared/Loader';
 
-import Navbar from './Components/Shared/Navbar/Navbar';
-import Login from './Components/User/Login';
-import Registration from './Components/User/Registration';
-import useAuth from './Hooks/useAuth';
-import PrivateRoute from './routes/PrivateRoute';
-// import Quizzes from './pages/Quizzes';
 
-const Quizzes = React.lazy(() => import('./pages/Quizzes'))
-const QuizDetails = React.lazy(() => import('./Components/Quiz/QuizDetails'))
-const PlayQuiz = React.lazy(() => import('./Components/Quiz/PlayQuiz'))
-const QuizResult = React.lazy(() => import('./Components/Quiz/QuizResult'))
+function counterReducer(state, action) {
+  switch (action) {
+    case "INCREMENT":
+      return state = state + 1;
 
+    case "DECREMENT":
+      return state = state - 1;
+  }
+}
 function App() {
-  const loggedInUser = useAuth(false)
+  const [counter, dispatch] = useReducer(counterReducer, 0)
 
   return (
-    <BrowserRouter>
-      <Navbar loggedInUser={loggedInUser} />
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Quizzes />} />
-          <Route path="/quiz/:id" element={<QuizDetails />} />
-          <Route path='/play/quiz' element={<PlayQuiz />} />
-          <Route path='/quiz/result' element={<PrivateRoute>
-            <QuizResult />
-          </PrivateRoute>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Registration />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+
+    <div>
+      {counter}
+      <button type='button' onClick={() => dispatch("INCREMENT")}>
+        Increment
+      </button>
+
+      <button type='button' onClick={() => dispatch("DECREMENT")}>
+        Decrement
+      </button>
+    </div>
   );
 }
 
