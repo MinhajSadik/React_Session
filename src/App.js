4a1b6721d71e7cfd25bc7bfbbd0e677eb28c417e
MinhajSadik <MinhajoSadik@gmail.com>
2023-01-28 21:49:37 +0600

    [DONE] practice code adding

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
 import React, { useReducer } from 'react';
 import './App.css';
 
+const initState = {
+  count: 0,
+  text: ""
+}
 
 function counterReducer(state, action) {
-  switch (action) {
+  switch (action.type) {
     case "INCREMENT":
-      return state = state + 1;
+      return {
+        count: state.count + 1
+      }
 
     case "DECREMENT":
-      return state = state - 1;
+      return {
+        count: state.count - 1,
+      }
+
+    case "INPUT_CHANGE":
+      return {
+        ...state,
+        text: action.payload.value
+      }
+    default:
+      return state
   }
 }
 function App() {
-  const [counter, dispatch] = useReducer(counterReducer, 0)
+  const [counter, dispatch] = useReducer(counterReducer, initState)
+
+  function onInputChange(e) {
+    const { name, value } = e.target
+    dispatch({ type: "INPUT_CHANGE", payload: { name, value } })
+  }
+
+  console.log(counter)
 
   return (
-
     <div>
-      {counter}
-      <button type='button' onClick={() => dispatch("INCREMENT")}>
+      <input type="text" onChange={onInputChange} />
+      {counter.count}
+      <br />
+      <button onClick={() => dispatch({
+        type: "INCREMENT"
+      })}>
         Increment
       </button>
 
-      <button type='button' onClick={() => dispatch("DECREMENT")}>
+      <button type='button' onClick={() => dispatch({
+        type: "DECREMENT"
+      })}>
         Decrement
       </button>
+
     </div>
   );
 }
---
+++ src/hooks/useMyReducer.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+
+export function useMyReducer(reducer, initState) {
+    const [state, setState] = useState(initState);
+
+    function dispatch(action) {
+        const newState = reducer(state, action)
+        setState(newState);
+    }
+
+    return [state, dispatch]
+}
+
